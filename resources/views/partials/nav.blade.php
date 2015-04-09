2c9f9fa6778d9126b8f621fe2884a8f2496b7fc8
Joseph Lyons <jclyons52@gmail.com>
2015-04-09 06:10:11 +0000

    moved users roles and permissions to admin dropdown menu

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -13,9 +13,15 @@
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li><a href="{{ url('/') }}">Home</a></li>
-					<li><a href="{{ url('roles') }}">Roles</a></li>
-					<li><a href="{{ url('users') }}">Users</a></li>
-                    <li><a href="{{ url('permissions') }}">Permissions</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">User Management<span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('users') }}">Users</a></li>
+                            <li><a href="{{ url('roles') }}">Roles</a></li>
+                            <li><a href="{{ url('permissions') }}">Permissions</a></li>
+                        </ul>
+                    </li>
+                    
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
