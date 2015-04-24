e7c9ae1fd023a12002d2042b7d9bcc13a3f1c15e
Joseph Lyons <jclyons52@gmail.com>
2015-04-24 21:10:08 +1000

    capitalised the p

--- config/app.php
+++ config/app.php
@@ -144,7 +144,7 @@ return [
 		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
-        'App\providers\ViewComposerServiceProvider',
+        'App\Providers\ViewComposerServiceProvider',
         'Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider',
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Html\HtmlServiceProvider',
