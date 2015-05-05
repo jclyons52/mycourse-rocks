ea6748bb27d181e7bc25c49609275ac46602136c
Joseph Lyons <jclyons52@gmail.com>
2015-05-05 10:07:04 +1000

    added google oauth

--- config/services.php
+++ config/services.php
@@ -44,6 +44,12 @@ return [
     'client_id' => env('FACEBOOK_CLIENT_ID'),
     'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
     'redirect' => 'http://mycourse.rocks/login/facebook',
+    ],
+
+    'google' => [
+    'client_id' => env('GOOGLE_CLIENT_ID'),
+    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+    'redirect' => 'http://mycourse.rocks/login/google',
 ]
 
 ];
