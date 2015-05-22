25b502fe5d0df2c09dbf7baa6e552b144d059b81
Joseph Lyons <jclyons52@gmail.com>
2015-05-22 23:59:12 +1000

    added simple social sharing buttons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -18,11 +18,34 @@
                 <div class="col-lg-3  col-md-3 col-sm-6 col-xs-12 ">
                     <h3> Find us on social media </h3>
                     <ul class="social">
-                        <li> <a href="#"> <i class=" fa fa-facebook">   </i> </a> </li>
-                        <li> <a href="#"> <i class="fa fa-twitter">   </i> </a> </li>
-                        <li> <a href="#"> <i class="fa fa-google-plus">   </i> </a> </li>
-                        <li> <a href="#"> <i class="fa fa-pinterest">   </i> </a> </li>
-                        <li> <a href="#"> <i class="fa fa-youtube">   </i> </a> </li>
+
+                    <!-- Facebook -->
+<li><a href="http://www.facebook.com/sharer.php?u=http://mycourse.rocks" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" /></a></li>
+ 
+<!-- Twitter -->
+<li><a href="http://twitter.com/share?url=http://mycourse.rocks&text=My Course Rocks&hashtags=mycourserocks" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" /></a></li>
+ 
+<!-- Google+ -->
+<li><a href="https://plus.google.com/share?url=http://mycourse.rocks" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/google.png" alt="Google" /></a></li>
+ 
+<!-- Digg -->
+<li><a href="http://www.digg.com/submit?url=http://mycourse.rocks" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/diggit.png" alt="Digg" /></a></li>
+ 
+<!-- Reddit -->
+<li><a href="http://reddit.com/submit?url=http://mycourse.rocks&title=My Course Rocks" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/reddit.png" alt="Reddit" /></a></li>
+ 
+<!-- LinkedIn -->
+<li><a href="http://www.linkedin.com/shareArticle?mini=true&url=http://mycourse.rocks" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/linkedin.png" alt="LinkedIn" /></a></li>
+ 
+<!-- Pinterest -->
+<li><a href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());"><img src="http://www.simplesharebuttons.com/images/somacro/pinterest.png" alt="Pinterest" /></a></li>
+ 
+<!-- StumbleUpon-->
+<li><a href="http://www.stumbleupon.com/submit?url=http://mycourse.rocks&title=My Course Rocks" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/stumbleupon.png" alt="StumbleUpon" /></a></li>
+ 
+<!-- Email -->
+<li><a href="mailto:?Subject=My Course Rocks&Body=I%20saw%20this%20and%20thought%20of%20you!%20 http://www.simplesharebuttons.com"><img src="http://www.simplesharebuttons.com/images/somacro/email.png" alt="Email" /></a></li>
+
                     </ul>
                 </div>
             </div>
