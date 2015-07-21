71fb588e3b20a94243d3fa1e6eb8d7c39bf20e51
Joseph Lyons <jclyons52@gmail.com>
2015-07-21 12:29:30 +1000

    old changes to lessosn view i didn't commit

--- resources/views/site/lessons/index.blade.php
+++ resources/views/site/lessons/index.blade.php
@@ -4,38 +4,39 @@
             <div class="well text-center">No Lessons found. <a class="btn btn-primary btn-xs"  href="{!! route('lessons.create',[$product->id]) !!}"><i class="glyphicon glyphicon-plus" ></i> Add New</a></div>
 
         @else
-            <table class="table">
-                <thead>
-                <th>Name</th>
-                <th>Score</th>
-                <th width="50px">Action</th>
-                </thead>
-                <tbody>
+
                 @foreach($product->lessons as $lesson)
-                    <tr>
+                    <div class="row">
+                        <div class="col-sm-4 text-center">
+                            <div class="row">
+                                <h3>{!! $lesson->name.' ' !!}</h3>
+                            </div>
+                            <div class="row">
+                                {{$lesson->userScore(Auth::id())}} / {{$lesson->quizzes->count()}}
 
-                        <td> <a href="{!! url('lesson/'. $lesson->id) !!}" class="btn btn-xs btn-primary">{!! $lesson->name.' ' !!}<i class="glyphicon glyphicon-arrow-right"></i> </a></td>
+                                    @if(Auth::check() && ($product->isOwnedBy(Auth::user()) or $product->isModeratedBy(Auth::user()) ))
+                                        <a href="{!! route('lessons.edit', [$lesson->id]) !!}"><i class="glyphicon glyphicon-edit"></i></a>
+                                        {{--<a href="{!! route('lessons.delete', [$lesson->id]) !!}" onclick="return confirm('Are you sure wants to delete this Lesson?')"><i class="glyphicon glyphicon-remove"></i></a>--}}
+                                    @endif
 
-                        <td>{{$lesson->userScore(Auth::id())}} / {{$lesson->quizzes->count()}}</td>
-                        <td>
-                            @if(Auth::check() && ($product->isOwnedBy(Auth::user()) or $product->isModeratedBy(Auth::user()) ))
-                                <a href="{!! route('lessons.edit', [$lesson->id]) !!}"><i class="glyphicon glyphicon-edit"></i></a>
-                                {{--<a href="{!! route('lessons.delete', [$lesson->id]) !!}" onclick="return confirm('Are you sure wants to delete this Lesson?')"><i class="glyphicon glyphicon-remove"></i></a>--}}
-                            @endif
-                        </td>
-                    </tr>
+
+                            </div>
+
+                        </div>
+                        <div class="col-sm-8">
+                            <br/>
+                                @foreach($lesson->links as $link)
+                                    <div class="col-sm-6"><a href="{!! url('lesson/'. $lesson->id.'?'.$link->id) !!}" >{!! $link->title !!}</a></div>
+                                @endforeach
+                        </div>
+                    </div>
+                <hr/>
                 @endforeach
                 @if(Auth::check() && ($product->isOwnedBy(Auth::user()) or $product->isModeratedBy(Auth::user()) ))
-                    <tr>
-                        <td>
                             <a class="btn btn-primary btn-xs"  href="{!! route('lessons.create',[$product->id]) !!}"><i class="glyphicon glyphicon-plus" ></i> Add New</a>
-                        </td>
-                    </tr>
 
                 @endif
-                </tbody>
 
-            </table>
         @endif
     </div>
 
